cc589b3e5fcec25541c10e28025c64bde7fd7147
thebigG <yunior.eury@gmail.com>
2022-10-13 22:02:25 -0500

    -restautant module

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,70 @@
+fn deliver_order(){}
+
+mod front_of_house{
+    pub mod hosting{
+        pub fn add_to_waitlist() {}
+
+        // fn seat_at_table() {}
+
+        fn fix_incorrect_order(){
+            cook_order();
+            super::super::deliver_order();
+        }
+
+        fn cook_order(){}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+mod back_of_house{
+        pub struct Breakfast{
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    //all variants in enums are public by default, opposite to structs.
+    pub enum Appetizer {
+    Soup,
+    Salad,
+    }
+}
+
+//idiomatic use of paths
+use crate::front_of_house::hosting;
+//This exposes the "use" statement to the outside world
+//pub use crate::front_of_house::hosting;
+// use crate::front_of_house::hosting as hs;
+
+//Convenient syntax
+//use std::io::{self, Write};
+pub fn eat_at_restaurant(){
+    //crate refers to the root of the module tree--the start of the crate itself
+    //full path
+    // crate::front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    //relative path
+    //  hs::add_to_waitlist();
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+}
+
+
