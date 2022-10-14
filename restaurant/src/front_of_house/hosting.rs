2d8fee5340b90746369b448030e70d37c8cd1e10
thebigG <yunior.eury@gmail.com>
2022-10-13 22:29:03 -0500

    -Modules in different files

---
+++ restaurant/src/front_of_house/hosting.rs
@@ -0,0 +1,10 @@
+pub fn add_to_waitlist() {}
+
+// fn seat_at_table() {}
+
+fn fix_incorrect_order() {
+    cook_order();
+    super::super::deliver_order();
+}
+
+fn cook_order() {}
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,30 +1,9 @@
-fn deliver_order(){}
+fn deliver_order() {}
 
-mod front_of_house{
-    pub mod hosting{
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        // fn seat_at_table() {}
-
-        fn fix_incorrect_order(){
-            cook_order();
-            super::super::deliver_order();
-        }
-
-        fn cook_order(){}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-mod back_of_house{
-        pub struct Breakfast{
+mod back_of_house {
+    pub struct Breakfast {
         pub toast: String,
         seasonal_fruit: String,
     }
@@ -40,8 +19,8 @@ mod back_of_house{
 
     //all variants in enums are public by default, opposite to structs.
     pub enum Appetizer {
-    Soup,
-    Salad,
+        Soup,
+        Salad,
     }
 }
 
@@ -53,7 +32,7 @@ use crate::front_of_house::hosting;
 
 //Convenient syntax
 //use std::io::{self, Write};
-pub fn eat_at_restaurant(){
+pub fn eat_at_restaurant() {
     //crate refers to the root of the module tree--the start of the crate itself
     //full path
     // crate::front_of_house::hosting::add_to_waitlist();
@@ -64,7 +43,4 @@ pub fn eat_at_restaurant(){
     //  hs::add_to_waitlist();
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
-
 }
-
-
