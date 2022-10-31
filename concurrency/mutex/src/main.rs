6fb305649dbb04c58a3fcf91116e0039fdf17949
thebigG <yunior.eury@gmail.com>
2022-10-30 23:11:35 -0500

    -Mutex.

--- concurrency/mutex/src/main.rs
+++ concurrency/mutex/src/main.rs
@@ -1,18 +1,33 @@
-use std::sync::{LockResult, Mutex};
+use std::rc::Rc;
+use std::sync::{Arc, LockResult, Mutex};
 use std::thread;
 
 fn main() {
-    let counter = Mutex::new(0);
+    // Arc is an atomic(thread-safe) reference-counted reference
+    let counter = Arc::new(Mutex::new(0));
 
-    let mut handles:Vec<i32> = vec![];
+    let mut handles: Vec<_> = vec![];
 
-    for _ in 0..10{
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut numResult = counter.lock();
-            let counter = match numResult{
-                Ok(num) => {num}
-                Err(lock) => {panic!("")}
+            let mut counter = match numResult {
+                Ok(num) => num,
+                Err(lock) => {
+                    panic!("")
+                }
             };
+
+            *counter += 1;
         });
+
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
