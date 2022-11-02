b6eab392f7e666a67d436b56f7c0f18c7334ae28
thebigG <yunior.eury@gmail.com>
2022-11-01 23:08:25 -0500

    -Draw trait.

--- oop/gui/src/main.rs
+++ oop/gui/src/main.rs
@@ -1,3 +1,37 @@
+use gui::{Button, Draw, Screen};
+
+struct SelectBox{
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+
+}
+
+impl Draw for SelectBox{
+    fn draw(&self) {
+        //draw code...
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen{
+        components: vec![
+            Box::new(SelectBox{
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ]
+            }),
+          Box::new(Button{
+              width: 50,
+              length: 10,
+              label: String::from("Ok")
+          })
+        ]
+    };
+
+    screen.run();
 }
