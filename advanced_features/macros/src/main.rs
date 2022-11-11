21b48e0e916b08e8de8337b591480afc1df2ac8c
thebigG <yunior.eury@gmail.com>
2022-11-11 08:03:13 -0600

    -Macros WIP. Declarative macros.

---
+++ advanced_features/macros/src/main.rs
@@ -0,0 +1,23 @@
+//declarative macro
+#[macro_export]
+macro_rules! add_iter {
+    (  $x:expr ) => {{
+        let mut sum = 0;
+        for item in $x {
+            sum += item;
+        }
+
+        sum
+        // let mut temp_vec = Vec::new();
+        // $(
+        //     temp_vec.push($x);
+        // )*
+        // temp_vec
+    }};
+}
+
+fn main() {
+    let list = vec![1, 2, 3];
+    let result = add_iter![list];
+    println!("Declarative add_iter macro:{result}");
+}
